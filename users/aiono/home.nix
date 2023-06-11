21a6fbc807233e5ae4062f801b37247dd36819dd
Aiono <sahinonur2000@hotmail.com>
2023-06-11 19:39:33 +0300

    feat: add vs code

--- users/aiono/home.nix
+++ users/aiono/home.nix
@@ -40,7 +40,10 @@
     pkgs.nano
     pkgs.blackbox-terminal
     pkgs.webtorrent_desktop
+    pkgs.vscode
   ];
+
+  nixpkgs.config.allowUnfree = true;
 
   # Home Manager is pretty good at managing dotfiles. The primary way to manage
   # plain files is through 'home.file'.
