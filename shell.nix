eaa7bdcfe63702d965801d157df927716ceb9e3c
Aiono <sahinonur2000@hotmail.com>
2023-08-25 23:44:46 +0300

    refactor: use paths

--- shell.nix
+++ shell.nix
@@ -1,14 +1,17 @@
-{ pkgs ? import (fetchTarball "channel:nixos-23.05") {} }:
-  pkgs.mkShell {
-    nativeBuildInputs = with pkgs; [ 
-      nushell
-      nixfmt
-    ];
-    shellHook = ''
-      export PROJECT_ROOT="$PWD"
+{ pkgs ? import (fetchTarball "channel:nixos-23.05") { } }:
+let
+  project_root = builtins.toString ./.;
+  scripts_dir = builtins.toString ./scripts;
+  users_dir = builtins.toString ./users;
+in pkgs.mkShell {
+  nativeBuildInputs = with pkgs; [ nushell nixfmt ];
+  shellHook = ''
+    export PROJECT_ROOT=${project_root};
 
-      alias format='nixfmt users/**/*.nix'
+    function format {
+      nixfmt ${users_dir}/**/*.nix shell.nix;
+    }
 
-      chmod +x ./scripts/set-user.nu
-    '';
-  }
+    chmod +x ${scripts_dir}/set-user.nu;
+  '';
+}
