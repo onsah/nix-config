67229b344f3de1867322f0e7bb9471cf59292340
Onur Şahin <onur@carbonhealth.com>
2023-10-28 14:27:50 +0200

    chore: simplfy

--- scripts/setup.nu
+++ scripts/setup.nu
@@ -1,4 +1,5 @@
 #!/usr/bin/env nu
 def main [] {
     git config core.hooksPath hooks;
+    chmod +x $"($env.SCRIPTS_DIR)/*"
 }
--- shell.nix
+++ shell.nix
@@ -1,11 +1,2 @@
 { pkgs ? import (fetchTarball "channel:nixos-23.05") { } }:
-let
-  project_root = builtins.toString ./.;
-  scripts_dir = builtins.toString ./scripts;
-  users_dir = builtins.toString ./users;
-in pkgs.mkShell {
-  nativeBuildInputs = with pkgs; [ nushell nixfmt ];
-  shellHook = ''
-    export PROJECT_ROOT=${project_root};
-  '';
-}
+pkgs.mkShell { nativeBuildInputs = with pkgs; [ nushell nixfmt ]; }
