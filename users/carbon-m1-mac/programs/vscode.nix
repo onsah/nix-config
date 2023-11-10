d69f337931455980e8c02e999e41e35e480303ca
Onur Şahin <onur@carbonhealth.com>
2023-11-10 13:39:19 +0100

    feat: nix extension settings

--- users/carbon-m1-mac/programs/vscode.nix
+++ users/carbon-m1-mac/programs/vscode.nix
@@ -1,11 +1,15 @@
 { ... }:
-
-{
+let
+  nixSettings = {
+    "nix.enableLanguageServer" = true;
+    "nix.serverPath" = "nil";
+  };
+in {
   programs.vscode = {
     enable = true;
     userSettings = {
       "terminal.integrated.fontFamily" = "'CaskaydiaCove Nerd Font Mono'";
       "terminal.integrated.defaultProfile.osx" = "zsh";
-    };
+    } // nixSettings;
   };
 }
