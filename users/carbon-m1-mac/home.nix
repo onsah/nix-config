0f570249cb3d18bfa256ec82daa2c7ddbae382b8
Onur Şahin <onur@carbonhealth.com>
2023-11-20 12:43:57 +0100

    feat: nnn and ncd

--- users/carbon-m1-mac/home.nix
+++ users/carbon-m1-mac/home.nix
@@ -33,7 +33,7 @@
           hash = "sha256-tB7SzUkAvJFW6h/nBKdh1pUz4zGih6+9O7DOs894xU4=";
         };
       });
-    in [ gnumake subversion myD2 typst graphviz nil lf ] ++ fontPackages;
+    in [ gnumake subversion myD2 typst graphviz nil lf nnn ] ++ fontPackages;
 
   home.sessionVariables = {
     # Some aws commands need this. It's okay to default to us-east-1 because that's where our alpha infra lies.
