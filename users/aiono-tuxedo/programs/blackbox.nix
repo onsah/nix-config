f8a7b450949436df3fa75e828ae9b7f51d6de741
Onur Sahin <sahinonur2000@hotmail.com>
2023-12-20 12:14:35 +0100

    feat(aiono-tuxedo): font size

--- users/aiono-tuxedo/programs/blackbox.nix
+++ users/aiono-tuxedo/programs/blackbox.nix
@@ -4,10 +4,12 @@
   home.packages = [ pkgs.blackbox-terminal ];
 
   dconf.settings = {
-    # Set padding
     "com/raggesilver/BlackBox" = with lib.gvariant; {
+      # Set padding to 10 for all directions
       terminal-padding =
         mkTuple [ (mkUint32 10) (mkUint32 10) (mkUint32 10) (mkUint32 10) ];
+      # Set font size to 10
+      font = "DejaVu Sans Mono 10";
     };
   };
 }
