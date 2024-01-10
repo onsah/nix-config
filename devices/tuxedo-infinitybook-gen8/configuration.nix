fc73a4921fae6364bd6cbbd6569925014b49f597
Onur Sahin <sahinonur2000@hotmail.com>
2024-01-10 12:11:55 +0100

    feat: nix auto gc

--- devices/tuxedo-infinitybook-gen8/configuration.nix
+++ devices/tuxedo-infinitybook-gen8/configuration.nix
@@ -136,4 +136,10 @@
   # https://nixos.wiki/wiki/Storage_optimization#Automatic
   nix.settings.auto-optimise-store = true;
 
+  nix.gc = {
+    automatic = true;
+    dates = "weekly";
+    options = "--delete-older-than 7d";
+  };
+
 }
