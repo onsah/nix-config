7e4a74bfcea48c5fda4060b28822bafc07cf5071
Onur Sahin <sahinonur2000@hotmail.com>
2024-01-22 13:36:10 +0100

    feat: add xits-math

--- users/aiono-tuxedo/fonts.nix
+++ users/aiono-tuxedo/fonts.nix
@@ -3,6 +3,8 @@
 {
   home.packages = [
     (pkgs.nerdfonts.override { fonts = [ "CascadiaCode" "DejaVuSansMono" ]; })
+    # I use this for my typst formulas
+    pkgs.xits-math
   ];
 
   fonts.fontconfig.enable = true;
