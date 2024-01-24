a3b77b310ba36af819ed3b94ad04381d1fcf4ab1
Onur Sahin <sahinonur2000@hotmail.com>
2024-01-24 12:50:27 +0100

    feat: manage tailor profiles

--- devices/tuxedo-infinitybook-gen8/tuxedo-control/default.nix
+++ devices/tuxedo-infinitybook-gen8/tuxedo-control/default.nix
@@ -1,5 +1,18 @@
-{ ... }:
+{ lib, ... }:
 
-{
+let fileset = lib.fileset;
+in {
   hardware.tuxedo-rs = { enable = true; };
+
+  # See `Managing profiles` section in README
+  environment.etc = {
+    "tailord/profiles".source = fileset.toSource {
+      root = ./etc/profiles;
+      fileset = ./etc/profiles;
+    };
+    "tailord/fan".source = fileset.toSource {
+      root = ./etc/fan;
+      fileset = ./etc/fan;
+    };
+  };
 }
