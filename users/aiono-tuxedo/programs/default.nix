8f2a721ec1921dfa37d84ff5e38e09ded34022fa
Onur Sahin <sahinonur2000@hotmail.com>
2024-03-21 20:18:13 +0100

    feat: shortwave

--- users/aiono-tuxedo/programs/default.nix
+++ users/aiono-tuxedo/programs/default.nix
@@ -1,6 +1,12 @@
 { ... }:
 
 {
-  imports =
-    [ ./blackbox.nix ./obsidian.nix ./vscode.nix ./git.nix ./newsflash.nix ];
+  imports = [
+    ./blackbox.nix
+    ./obsidian.nix
+    ./vscode.nix
+    ./git.nix
+    ./newsflash.nix
+    ./shortwave.nix
+  ];
 }
