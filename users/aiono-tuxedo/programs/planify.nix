668f46c643899d86bef0c618686057c94774433b
Onur Sahin <sahinonur2000@hotmail.com>
2024-06-10 20:04:22 +0200

    feat: alias planify

--- users/aiono-tuxedo/programs/planify.nix
+++ users/aiono-tuxedo/programs/planify.nix
@@ -1,4 +1,10 @@
-{ ... }:
+{ pkgs, ... }:
 
 let unstablePkgs = import <nixpkgs-unstable> { };
-in { home.packages = [ unstablePkgs.planify ]; }
+in {
+  home.packages = [
+    unstablePkgs.planify
+    (pkgs.writeShellScriptBin "tasks"
+      "${unstablePkgs.planify}/bin/io.github.alainm23.planify")
+  ];
+}
