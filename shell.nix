d3b1bf0df740001b778821b3502ae85310200647
Onur Sahin <sahinonur2000@hotmail.com>
2024-06-19 17:32:07 +0200

    chore: update shell.nix

--- shell.nix
+++ shell.nix
@@ -1,2 +1,12 @@
-{ pkgs ? import (fetchTarball "channel:nixos-23.05") { } }:
-pkgs.mkShell { nativeBuildInputs = with pkgs; [ nushell nixfmt ]; }
+{
+  pkgs ?
+    import
+      (fetchTarball "https://github.com/NixOS/nixpkgs/tarball/e381a1288138aceda0ac63db32c7be545b446921")
+      { },
+}:
+pkgs.mkShell {
+  nativeBuildInputs = with pkgs; [
+    nushell
+    nixfmt-rfc-style
+  ];
+}
