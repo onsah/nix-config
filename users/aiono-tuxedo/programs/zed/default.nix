1ec5e0b61eec01b4dd34ad8eb00871b0e68d6c14
Onur Sahin <sahinonur2000@hotmail.com>
2024-07-28 12:44:30 +0200

    fix: zed with python support

--- users/aiono-tuxedo/programs/zed/default.nix
+++ users/aiono-tuxedo/programs/zed/default.nix
@@ -1,13 +1,31 @@
-{ ... }:
+{ pkgs, ... }:
 
 let
-  unstablePkgs = import <nixpkgs-unstable> { };
+  stagingNextPkgs =
+    import
+      (fetchTarball "https://github.com/NixOS/nixpkgs/tarball/614efe64e87d1450428e4fde0024eb784d06ba20")
+      { };
+  # This lets zed to see installed language servers.
+  zed-fhs = stagingNextPkgs.buildFHSUserEnv {
+    name = "zed";
+    targetPkgs = pkgs: [ pkgs.zed-editor ];
+    runScript = "zed";
+  };
 in
 {
-  home.packages = [ unstablePkgs.zed-editor ];
+  home.packages = [ zed-fhs ];
 
   xdg.configFile = {
     "zed/settings.json".source = ./settings.json;
     "zed/keymap.json".source = ./keymap.json;
+  };
+
+  home.file = {
+    # zed checks here for language servers
+    # For reproducibility I install them with nix rather than leaving it to zed.
+    ".local/share/zed/languages/pyright" = {
+      enable = true;
+      source = "${pkgs.pyright}/lib";
+    };
   };
 }
