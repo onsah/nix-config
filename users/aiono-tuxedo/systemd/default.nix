9ab8098dbe4b73180fc61914dc9ca7605ae10fea
Onur Sahin <sahinonur2000@hotmail.com>
2024-09-02 21:18:17 +0200

    feat: update note location

--- users/aiono-tuxedo/systemd/default.nix
+++ users/aiono-tuxedo/systemd/default.nix
@@ -1,28 +1,24 @@
 { pkgs, lib, ... }:
 
 let
-  nextcloud-sync-service =
-    import ../../common/systemd/services/nextcloud-sync-service.nix;
-in {
-  systemd.user =
-    (lib.lists.foldl (lib.attrsets.recursiveUpdate) { startServices = true; } [
+  nextcloud-sync-service = import ../../common/systemd/services/nextcloud-sync-service.nix;
+in
+{
+  systemd.user = (
+    lib.lists.foldl (lib.attrsets.recursiveUpdate) { startServices = true; } [
       (nextcloud-sync-service {
         inherit pkgs;
         name = "cyber-physical-notes";
         description = "Cyber Physical Notes";
-        nextcloudPath =
-          "/Freiburg/Courses/Fall Semester/Cyber-Physical Systems: Discrete Models";
-        localPath =
-          "/home/aiono/Documents/Master/Courses/Cyber-Physical Systems/Notes";
+        nextcloudPath = "/Freiburg/Courses/Fall Semester/Cyber-Physical Systems: Discrete Models";
+        localPath = "/home/aiono/Documents/Master/Courses/Cyber-Physical Systems/Notes";
       })
       (nextcloud-sync-service {
         inherit pkgs;
         name = "verification-of-digital-circuits-notes";
         description = "Verification of Digital Circuits Notes";
-        nextcloudPath =
-          "/Freiburg/Courses/Fall Semester/Verification of Digital Circuits/Notes";
-        localPath =
-          "/home/aiono/Documents/Master/Courses/Verification of Digital Circuits/Notes";
+        nextcloudPath = "/Freiburg/Courses/Fall Semester/Verification of Digital Circuits/Notes";
+        localPath = "/home/aiono/Documents/Master/Courses/Verification of Digital Circuits/Notes";
       })
       (nextcloud-sync-service {
         inherit pkgs;
@@ -36,7 +32,7 @@ in {
         name = "politics-notes";
         description = "Politics Notes";
         nextcloudPath = "/Politics";
-        localPath = "/home/aiono/Documents/NextcloudSynced/Politics";
+        localPath = "/home/aiono/Documents/NextcloudSynced/Politics/Notes";
       })
       (nextcloud-sync-service {
         inherit pkgs;
@@ -49,10 +45,9 @@ in {
         inherit pkgs;
         name = "software-engineering-notes";
         description = "Software Engineering Course Notes";
-        nextcloudPath =
-          "/Freiburg/Courses/Spring Semester/Software Engineering";
-        localPath =
-          "/home/aiono/Documents/Master/Courses/software-engineering/Notes";
+        nextcloudPath = "/Freiburg/Courses/Spring Semester/Software Engineering";
+        localPath = "/home/aiono/Documents/Master/Courses/software-engineering/Notes";
       })
-    ]);
+    ]
+  );
 }
