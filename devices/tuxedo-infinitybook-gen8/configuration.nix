c2c4631bad2e93d3a7e1cb421f801e787fbaf839
Onur Sahin <sahinonur2000@hotmail.com>
2024-09-21 00:55:25 +0300

    feat: remove time zone config so it can be set dynamically

--- devices/tuxedo-infinitybook-gen8/configuration.nix
+++ devices/tuxedo-infinitybook-gen8/configuration.nix
@@ -32,9 +32,6 @@
   # Enable networking
   networking.networkmanager.enable = true;
 
-  # Set your time zone.
-  time.timeZone = "Europe/Berlin";
-
   # Select internationalisation properties.
   i18n.defaultLocale = "en_US.UTF-8";
 
