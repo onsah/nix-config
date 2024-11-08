16e8069dd688eaa473bf76cdbb95f727ab3d8e3f
onur sahin <sahinonur2000@hotmail.com>
2024-11-08 15:51:12 +0000

    feat: sync notes to Nextcloud

--- users/cps-tutor/home.nix
+++ users/cps-tutor/home.nix
@@ -4,6 +4,7 @@
   
   imports = [
     ../common/programs
+    ./systemd
   ];
 
   # Home Manager needs a bit of information about you and the paths it should
---
+++ users/cps-tutor/systemd/default.nix
@@ -0,0 +1,25 @@
+{ pkgs, lib, ... }:
+
+let
+  nextcloud-sync-service = import ../../common/systemd/services/nextcloud-sync-service.nix;
+in
+{
+  systemd.user = (
+    lib.lists.foldl (lib.attrsets.recursiveUpdate) { startServices = true; } [
+      (nextcloud-sync-service {
+        inherit pkgs;
+        name = "programming-notes";
+        description = "Programming Notes";
+        nextcloudPath = "/Programming/Notes";
+        localPath = "/mnt/c/Users/Tutor/Documents/ProgrammingNotes";
+      })
+      (nextcloud-sync-service {
+        inherit pkgs;
+        name = "cps-tutoring-notes";
+        description = "CPS Tutoring Notes";
+        nextcloudPath = "/CPS Tutoring";
+        localPath = "/mnt/c/Users/Tutor/Documents/CPS-Grading";
+      })
+    ]
+  );
+}
