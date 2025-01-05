02a501c4683a76c1ad0dfe99408151ce1a2c4a95
Onur Sahin <sahinonur2000@hotmail.com>
2025-01-05 16:45:14 +0100

    refactor: simplify readeck deriation

--- devices/hetzner-nixos/readeck/readeck.nix
+++ devices/hetzner-nixos/readeck/readeck.nix
@@ -9,28 +9,20 @@
   ...
 }:
 
-let
-  readeckBinary = fetchurl {
+stdenv.mkDerivation {
+  pname = "readeck";
+  version = "0.16.0";
+
+  src = fetchurl {
     url = "https://codeberg.org/readeck/readeck/releases/download/0.16.0/readeck-0.16.0-linux-amd64";
     hash = "sha256-N+NVAQY5Kk423UhrVXKbjU7N6ftOv5Q8ZnrU3QIhpJA=";
   };
-  fs = lib.fileset;
-in
-stdenv.mkDerivation {
-  pname = "readeck";
-  version = "2.12.1";
 
-  src = fs.toSource {
-    root = ./.;
-    fileset = fs.unions [ ];
-  };
-
-  buildPhase = '''';
+  phases = [ "installPhase" ];
 
   installPhase = ''
-    mkdir -p $out
     mkdir -p $out/bin
-    cp ${readeckBinary} $out/bin/readeck
+    cp $src $out/bin/readeck
     chmod +x $out/bin/readeck
   '';
 }
