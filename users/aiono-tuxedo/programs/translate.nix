82027211c0f4e44edea7d4cb946d9707147028db
Onur Sahin <sahinonur2000@hotmail.com>
2025-03-15 19:20:00 +0300

    fix: temporary fix for translateLocally

--- users/aiono-tuxedo/programs/translate.nix
+++ users/aiono-tuxedo/programs/translate.nix
@@ -1,14 +1,26 @@
-{ ... }:
+{
+  pkgs,
+  lib,
+  hostPlatform,
+  ...
+}:
 
-let
-  pkgs = import <nixpkgs-unstable> { };
-in
 let
   de-en-model-name = "de-en-base";
 in
 {
   home.packages = [
-    pkgs.translatelocally
+    # Temporary fix until https://github.com/NixOS/nixpkgs/pull/385549
+    (pkgs.translatelocally.overrideAttrs {
+      cmakeFlags = [
+        "-DBLAS_LIBRARIES=-lblas"
+        "-DCBLAS_LIBRARIES=-lcblas"
+
+        # See the following for context:
+        # https://github.com/NixOS/nixpkgs/pull/385549
+        (lib.optionalString pkgs.hostPlatform.isx86_64 "-DBUILD_ARCH=x86-64")
+      ];
+    })
     pkgs.translatelocally-models.${de-en-model-name}
     (pkgs.writeShellScriptBin "translate-de-en" "echo $1 | ${pkgs.translatelocally}/bin/translateLocally -m ${de-en-model-name}")
   ];
