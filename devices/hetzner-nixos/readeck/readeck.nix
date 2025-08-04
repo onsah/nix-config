660ee3f6ae1a05166cb72fadeef75df50bff317b
Onur Sahin <sahinonur2000@hotmail.com>
2025-08-04 11:00:09 +0000

    fix: include readeck binary in the config for now because it's been removed from releases

--- devices/hetzner-nixos/readeck/readeck.nix
+++ devices/hetzner-nixos/readeck/readeck.nix
@@ -13,10 +13,11 @@ stdenv.mkDerivation {
   pname = "readeck";
   version = "0.16.0";
 
-  src = fetchurl {
-    url = "https://codeberg.org/readeck/readeck/releases/download/0.16.0/readeck-0.16.0-linux-amd64";
-    hash = "sha256-N+NVAQY5Kk423UhrVXKbjU7N6ftOv5Q8ZnrU3QIhpJA=";
-  };
+  # src = fetchurl {
+  #   url = "https://codeberg.org/readeck/readeck/releases/download/0.18.2/readeck-0.18.2-linux-amd64";
+  #   hash = "sha256-WYxfouw/hnwUEGTX66B+yc+m7hOx7worSTGP4ZkH/WA=";
+  #  };
+  src = ./readeck;
 
   phases = [ "installPhase" ];
 
