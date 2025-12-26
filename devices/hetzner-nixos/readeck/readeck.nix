037eb01d1aafeeafb63e36d1dbd5f5f56276aded
Onur Sahin <sahinonur2000@hotmail.com>
2025-12-26 16:11:42 +0100

    feat: Update readeck

--- devices/hetzner-nixos/readeck/readeck.nix
+++ devices/hetzner-nixos/readeck/readeck.nix
@@ -14,8 +14,8 @@ stdenv.mkDerivation {
   version = "0.20.0";
 
   src = fetchurl {
-    url = "https://codeberg.org/readeck/readeck/releases/download/0.20.0/readeck-0.20.0-linux-amd64";
-    hash = "sha256-mawDfx8jp9/hRK+CT+/vfmB1aamzqCKPb+FZW0SKZgY=";
+    url = "https://codeberg.org/readeck/readeck/releases/download/0.21.5/readeck-0.21.5-linux-amd64";
+    hash = "sha256-Q30HCHkG8ebTR73HkNXRfmtEEXT4WsOJcAxmeSm2JGU=";
   };
 
   phases = [ "installPhase" ];
