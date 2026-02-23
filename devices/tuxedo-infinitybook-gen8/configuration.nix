1a91c235aa8d4d409ebd04b08479c8f6eb8ce96c
Onur Sahin <sahinonur2000@hotmail.com>
2026-02-23 10:25:22 +0300

    chore: Update renamed option

--- devices/tuxedo-infinitybook-gen8/configuration.nix
+++ devices/tuxedo-infinitybook-gen8/configuration.nix
@@ -70,7 +70,7 @@
   services.printing.enable = true;
 
   # Enable sound with pipewire.
-  hardware.pulseaudio.enable = false;
+  services.pulseaudio.enable = false;
   security.rtkit.enable = true;
   services.pipewire = {
     enable = true;
@@ -79,6 +79,7 @@
     pulse.enable = true;
     # If you want to use JACK applications, uncomment this
     jack.enable = true;
+    wireplumber.enable = true;
 
     # use the example session manager (no others are packaged yet so this is enabled by default,
     # no need to redefine it in your config for now)
