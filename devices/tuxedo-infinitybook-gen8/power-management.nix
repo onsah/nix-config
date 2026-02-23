59957ae2a1d48f022249de9d9bdb71624517f343
Onur Sahin <sahinonur2000@hotmail.com>
2026-02-23 10:24:59 +0300

    feat: reduce timer tick interrupts

--- devices/tuxedo-infinitybook-gen8/power-management.nix
+++ devices/tuxedo-infinitybook-gen8/power-management.nix
@@ -3,4 +3,10 @@
 {
   services.thermald.enable = true;
   powerManagement.powertop.enable = true;
+
+  # Reduce timer tick interrupts for better battery life
+  boot.kernelParams = [
+    "nohz=on"
+    "nohz_full=1-19" # This device has 20 cores
+  ];
 }
