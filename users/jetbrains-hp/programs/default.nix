1a37d8100ec3e96e1622873e7340e41f745d62f4
Onur.Sahin <onur.sahin@jetbrains.com>
2026-06-26 16:15:50 +0200

    chore: update npins

--- users/jetbrains-hp/programs/default.nix
+++ users/jetbrains-hp/programs/default.nix
@@ -2,17 +2,41 @@
 {
   imports = [
     ../../common/programs
+    ../../common/programs/vscode.nix
+    ../../common/programs/wl-clipboard.nix
+    ../../common/programs/nushell
   ];
 
   programs.bash.profileExtra = ''
     if [ -e /home/Onur.Sahin/.nix-profile/etc/profile.d/nix.sh ]; then . /home/Onur.Sahin/.nix-profile/etc/profile.d/nix.sh; fi # added by Nix installer
   '';
 
-  nixpkgs.config.allowUnfreePredicate = pkg: builtins.elem (lib.getName pkg) [
-    "vscode" "obsidian"
-  ];
+  programs.bash.bashrcExtra = ''
+    export PATH=$HOME/.local/bin:$PATH;
+  '';
+
+  nixpkgs.config.allowUnfreePredicate =
+    pkg:
+    builtins.elem (lib.getName pkg) [
+      "vscode"
+      "obsidian"
+    ];
 
   home.packages = with pkgs; [
     scala-cli
+    opencode
+    codex
+    bun
+    nil
   ];
+
+  programs.vscode.package = pkgs.vscode.overrideAttrs (oldAttrs: {
+    buildInputs = (oldAttrs.buildInputs or [ ]) ++ [ pkgs.makeWrapper ];
+    postInstall =
+      (oldAttrs.postInstall or "")
+      + ''
+        wrapProgram $out/bin/code \
+          --add-flags "--no-sandbox"
+      '';
+  });
 }
